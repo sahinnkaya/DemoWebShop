c95799b3bda206af5014142d4b5e0653378cd6ac
sahinnkaya <kayasahin94@gmail.com>
2024-01-31 16:11:33 -0600

    Test

---
+++ src/Utilities/ReusableMethods.java
@@ -0,0 +1,13 @@
+package Utilities;
+
+public class ReusableMethods {
+
+    public static void myWait(int sec){
+        try{
+            Thread.sleep(sec * 1000);
+        }catch (InterruptedException e){
+            throw new RuntimeException();
+        }
+    }
+
+}
